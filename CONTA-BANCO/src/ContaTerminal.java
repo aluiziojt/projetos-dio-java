308528648869aff19ba683bf87128cc677f009d7
Aluizio <aluizio4090@gmail.com>
2025-01-31 10:05:16 -0300

    Projeto de conta bancária feita no bootcamp java. Ele pede nome, agência e saldo da conta e envia uma mensagem falando os dados e mostrando o valor para saque

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // usando a classe Scanner que o professor ensinou
+        Scanner scanner = new Scanner(System.in);
+
+        // pede o número da agência
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        // pede e lê o nome do cliente
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        // pede saldo da conta
+        System.out.print("Por favor, digite o saldo da Conta: ");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        // Exibe a mensagem final que foi pedida no projeto
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+                        + agencia + " e seu saldo " + saldo + " já está disponível para saque.";
+        System.out.println(mensagem);
     }
 }
